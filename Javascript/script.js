f46428cc5a238dbc48d594c27ae891c76dad5619
Rayane Ventura <rayyventura@gmail.com>
2021-11-04 21:37:07 -0300

    Cartas virando quando Clicadas

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,8 @@
 const quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
+while(quantidadeCartas%2 !==0 ){
+    quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
+}
+
+function cartaClicada (opcao){
+    opcao.classList.toggle('cartaClicada')
+}
