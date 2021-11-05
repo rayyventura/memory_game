4f3519735f90f763a18a4eb23b1b8e8420b59a00
Rayane Ventura <rayyventura@gmail.com>
2021-11-05 12:36:18 -0300

    Cartas embaralhadas, divididas em duas colunas

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,34 @@
+const images = [
+    'assets/metalparrot.gif',
+    'assets/explodyparrot.gif',
+    'assets/bobrossparrot.gif',
+    'assets/revertitparrot.gif',
+    'assets/fiestaparrot.gif',
+    'assets/tripletsparrot.gif',
+    'assets/unicornparrot.gif'
+]
+images.sort(comparador);
+function comparador(){
+    return Math.random() - 0.5;
+}
+const primeiraColuna = document.querySelector('.primeira-coluna');
+const segundaColuna = document.querySelector('.segunda-coluna');
+for (let i = 0; i < images.length; i++){
+
+primeiraColuna.innerHTML += ` 
+            <div class="card1 display-none card" onclick="cartaClicada(this)" >
+                <div class="front-face face"><img src="assets/cover.jpg" alt="card verde com um papagaio no centro"></div>
+                <div class="back-face face"><img src="${images[i]}"  alt="crazy parrot"></div>
+            </div>`
+segundaColuna.innerHTML +=`
+            <div class="card2 display-none card" onclick="cartaClicada(this)" >
+                <div class="front-face face"><img src="assets/cover.jpg" alt="card verde com um papagaio no centro"></div>
+                <div class="back-face face"><img src="${images[i]}"  alt="crazy parrot"></div>
+            </div>`
+}
+
+
+
 let quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
 while(quantidadeCartas%2 !==0 || quantidadeCartas<4 ){
     quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
