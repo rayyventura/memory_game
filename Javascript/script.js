c3dc10a0782b651c05c0a4a5737920e7dc400703
Rayane Ventura <rayyventura@gmail.com>
2021-11-05 23:38:19 -0300

    Projeto Finalizado,sem bônus (Selecionando 3 cartas+ em 1s)

--- Javascript/script.js
+++ Javascript/script.js
@@ -11,36 +11,35 @@ images.sort(comparador);
 function comparador(){
     return Math.random() -0.5;
 }
+
 const primeiraColuna = document.querySelector('.primeira-coluna');
 const segundaColuna = document.querySelector('.segunda-coluna');
-let quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
-while(quantidadeCartas%2 !==0 || quantidadeCartas<4 ){
-    quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Pare entre 4 e 14"));
+
+let quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um número Par entre 4 e 14"));
+while(quantidadeCartas%2 !==0 || quantidadeCartas<4 || quantidadeCartas>14){
+    quantidadeCartas = parseInt(prompt("Você não digitou um número Válido! Escolha um número PAR entre 4 e 14"));
 }
 let metadeQtdCartas = (quantidadeCartas/2);
 
+
+
 for (let i = 0; i < metadeQtdCartas; i++){
     primeiraColuna.innerHTML += ` 
-                <div class="card1 display-none card" onclick="cartaClicada(this)" >
+                <div class="card1 display-none card ${images[i]}"  onclick="cartaClicada(this)"  >
                     <div class="front-face face"><img src="assets/cover.jpg" alt="card verde com um papagaio no centro"></div>
                     <div class="back-face face"><img src="${images[i]}"  alt="crazy parrot"></div>
                 </div>`
 }
 for(let i = metadeQtdCartas-1; i >=0 ; i--){
     segundaColuna.innerHTML +=`
-            <div class="card2 display-none card" onclick="cartaClicada(this)" >
+            <div class="card2 display-none card ${images[i]}"  " onclick="cartaClicada(this)">
                 <div class="front-face face"><img src="assets/cover.jpg" alt="card verde com um papagaio no centro"></div>
                 <div class="back-face face"><img src="${images[i]}"  alt="crazy parrot"></div>
             </div>`
 }
 
-
-
-
-
 const cartas1 =  document.querySelectorAll(".card1");
 const cartas2 =  document.querySelectorAll(".card2");
-
 
 
 for(let i=0; i<metadeQtdCartas;i++){
@@ -49,8 +48,35 @@ for(let i=0; i<metadeQtdCartas;i++){
 for(let i = metadeQtdCartas-1; i >=0 ; i--){
     cartas2[i].classList.add("display");
 }
+let primeiraCarta, segundaCarta;
 
-
+contador=0;
 function cartaClicada (opcao){
-    opcao.classList.toggle('cartaClicada');
+    opcao.classList.add('carta-clicada');
+    if(primeiraCarta==null){
+        primeiraCarta = opcao;
+        return false;
+    }
+     segundaCarta=opcao;
+     setTimeout(cartaMatch,1000);
+     contador++;
+     setTimeout(jogoFinalizado,500);
+    }
+    
+function cartaMatch(){
+    const match=primeiraCarta.classList.item(3)===segundaCarta.classList.item(3);
+    if(!match){
+        primeiraCarta.classList.remove("carta-clicada");
+        segundaCarta.classList.remove("carta-clicada");
+    }
+    primeiraCarta=null;
+    segundaCarta=null;
+}
+function jogoFinalizado(){
+    const cartasClicadas1 = primeiraColuna.querySelectorAll(".carta-clicada").length;
+    const cartasClicadas2 = segundaColuna.querySelectorAll(".carta-clicada").length;
+    if(cartasClicadas1+cartasClicadas2===quantidadeCartas){
+        const mensagemDeVitoria = `Você ganhou em ${contador*2} jogadas!`;
+        alert(mensagemDeVitoria);
+    }
 }
